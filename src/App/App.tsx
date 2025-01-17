cc52aed9fd6205ef8e576284899eb30bc22c4d24
Thomas Youel <117925401+tomyouel@users.noreply.github.com>
2025-01-17 00:49:37 +0000

    fix/copy fix on GitHub

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -38,7 +38,7 @@ const UI: FC = () => {
               }
               style={{ cursor: 'pointer' }}
             >
-              Github
+              GitHub
             </RoundedModule>
             <RoundedModule
               onClick={() =>
